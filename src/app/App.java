00d3788a63860588bde684c079c66af2b2081326
Stefano Germano <stefanogermano0@gmail.com>
2019-03-12 17:37:10 +0000

    I/O exceptions checks

--- src/app/App.java
+++ src/app/App.java
@@ -59,59 +59,84 @@ public class App {
 
 		logger.info("Reading from: '" + basePath + "'");
 
-		Schema schema = IO.readSchemaAndDependenciesChaseBench(basePath, baseTest);
-		Dependency[] allDependencies = schema.getAllDependencies();
+		Dependency[] allDependencies = null;
+		Collection<TGD> queriesRules = null;
+		Collection<Atom> facts = null;
+		try {
 
-		logger.info("# Dependencies: " + allDependencies.length);
-		logger.trace(schema);
+			Schema schema = IO.readSchemaAndDependenciesChaseBench(basePath, baseTest);
+			allDependencies = schema.getAllDependencies();
 
-		Collection<Atom> facts = IO.readFactsChaseBench(basePath, fact_querySize, schema);
-		logger.info("# Facts: " + facts.size());
-		logger.trace(facts);
+			logger.info("# Dependencies: " + allDependencies.length);
+			logger.trace(schema);
 
-		Collection<TGD> queriesRules = IO.readQueriesChaseBench(basePath, fact_querySize, schema);
-		logger.info("# Queries: " + queriesRules.size());
-		logger.debug(queriesRules);
+			facts = IO.readFactsChaseBench(basePath, fact_querySize, schema);
+			logger.info("# Facts: " + facts.size());
+			logger.trace(facts);
 
-		Collection<TGD> guardedSaturation = GSat
-				.runGSat(ArrayUtils.addAll(allDependencies, queriesRules.toArray(new TGD[queriesRules.size()])));
-		logger.info("Rewriting completed!");
-		System.out.println("Guarded saturation:");
-		System.out.println("=========================================");
-		guardedSaturation.forEach(System.out::println);
-		System.out.println("=========================================");
+			queriesRules = IO.readQueriesChaseBench(basePath, fact_querySize, schema);
+			logger.info("# Queries: " + queriesRules.size());
+			logger.debug(queriesRules);
 
-		String baseOutputPath = "test" + File.separator + "datalog" + File.separator;
-		new File(baseOutputPath).mkdirs();
-		IO.writeDatalogRules(guardedSaturation, baseOutputPath + baseTest + ".rul");
-		IO.writeDatalogFacts(facts, baseOutputPath + baseTest + ".data");
+		} catch (Exception e) {
+			System.err.println("Data loading failed. The system will now terminate.");
+			System.exit(1);
+		}
 
-		System.out.println("Performing the full grounding...");
-		SolverOutput solverOutput = Logic.invokeSolver("executables" + File.separator + "idlv_1.1.3_windows_x86-64.exe",
-				"--t --no-facts --check-edb-duplication", // "dlv.mingw.exe", "-nofacts",
-				Arrays.asList(baseOutputPath + baseTest + ".rul", baseOutputPath + baseTest + ".data"));
-		// System.out.println(solverOutput);
-		System.out.println(
-				"Output size: " + solverOutput.getOutput().length() + ", " + solverOutput.getErrors().length());
-		IO.writeSolverOutput(solverOutput, baseOutputPath + baseTest + ".idlv.output" // ".dlv.output"
-		);
+		Collection<TGD> guardedSaturation = null;
+		try {
 
-		for (TGD query : queriesRules) {
+			guardedSaturation = GSat
+					.runGSat(ArrayUtils.addAll(allDependencies, queriesRules.toArray(new TGD[queriesRules.size()])));
+			logger.info("Rewriting completed!");
+			System.out.println("Guarded saturation:");
+			System.out.println("=========================================");
+			guardedSaturation.forEach(System.out::println);
+			System.out.println("=========================================");
 
-			IO.writeChaseBenchDatalogQueries(Arrays.asList(query), baseOutputPath + baseTest + "_queries.rul");
+		} catch (Exception e) {
+			System.err.println("Guarded Saturation algorithm failed. The system will now terminate.");
+			System.exit(1);
+		}
 
-			SolverOutput solverOutputQuery = Logic.invokeSolver(
-					"executables" + File.separator + "idlv_1.1.3_windows_x86-64.exe", "--query", // "dlv.mingw.exe",
-					// "-nofacts",
-					Arrays.asList(baseOutputPath + baseTest + ".rul", baseOutputPath + baseTest + ".data",
-							baseOutputPath + baseTest + "_queries.rul"));
-			// System.out.println(solverOutput2);
-			System.out.println("Output size: " + solverOutputQuery.getOutput().length() + ", "
-					+ solverOutputQuery.getErrors().length());
-			IO.writeSolverOutput(solverOutputQuery,
-					baseOutputPath + baseTest + "." + query.getHead().getAtoms()[0].getPredicate() + ".idlv.output" // ".dlv.output"
-			);
+		SolverOutput solverOutput = null;
+		try {
+			String baseOutputPath = "test" + File.separator + "datalog" + File.separator;
+			new File(baseOutputPath).mkdirs();
+			IO.writeDatalogRules(guardedSaturation, baseOutputPath + baseTest + ".rul");
+			IO.writeDatalogFacts(facts, baseOutputPath + baseTest + ".data");
 
+			System.out.println("Performing the full grounding...");
+			solverOutput = Logic.invokeSolver("executables" + File.separator + "idlv_1.1.3_windows_x86-64.exe",
+					"--t --no-facts --check-edb-duplication", // "dlv.mingw.exe", "-nofacts",
+					Arrays.asList(baseOutputPath + baseTest + ".rul", baseOutputPath + baseTest + ".data"));
+
+			// System.out.println(solverOutput);
+			System.out.println(
+					"Output size: " + solverOutput.getOutput().length() + ", " + solverOutput.getErrors().length());
+			IO.writeSolverOutput(solverOutput, baseOutputPath + baseTest + ".idlv.output"); // ".dlv.output"
+
+			for (TGD query : queriesRules) {
+
+				IO.writeChaseBenchDatalogQueries(Arrays.asList(query), baseOutputPath + baseTest + "_queries.rul");
+
+				SolverOutput solverOutputQuery = Logic.invokeSolver(
+						"executables" + File.separator + "idlv_1.1.3_windows_x86-64.exe", "--query",
+						// "dlv.mingw.exe", "-FC",
+						Arrays.asList(baseOutputPath + baseTest + ".rul", baseOutputPath + baseTest + ".data",
+								baseOutputPath + baseTest + "_queries.rul"));
+
+				// System.out.println(solverOutput2);
+				System.out.println("Output size: " + solverOutputQuery.getOutput().length() + ", "
+						+ solverOutputQuery.getErrors().length());
+				IO.writeSolverOutput(solverOutputQuery, baseOutputPath + baseTest + "."
+						+ query.getHead().getAtoms()[0].getPredicate() + ".idlv.output"); // ".dlv.output"
+
+			}
+
+		} catch (Exception e) {
+			System.err.println("Datalog solver execution failed. The system will now terminate.");
+			System.exit(1);
 		}
 
 		return solverOutput;
