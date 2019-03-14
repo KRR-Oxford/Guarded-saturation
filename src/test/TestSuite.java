b9c39a3575e3d01ecbfa9d59c61fad310589d9ba
Stefano Germano <stefanogermano0@gmail.com>
2019-03-14 16:03:17 +0000

    Test Suite

---
+++ src/test/TestSuite.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({ GSatTest.class, LogicTest.class, IRISPMTest.class, ChaseBenchTests.class })
+
+public class TestSuite {
+}
