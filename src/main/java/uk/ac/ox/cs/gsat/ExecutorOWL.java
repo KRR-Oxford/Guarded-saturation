d853704d5e4a8b44c12ad733329b57267bd85cef
Maxime Buron <maxime.buron@inria.fr>
2021-05-11 11:21:17 +0100

    kaon2 owl executor

--- src/main/java/uk/ac/ox/cs/gsat/ExecutorOWL.java
+++ src/main/java/uk/ac/ox/cs/gsat/ExecutorOWL.java
@@ -1,10 +1,10 @@
 package uk.ac.ox.cs.gsat;
 
-import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Locale;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -17,6 +17,7 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.semanticweb.kaon2.api.Axiom;
 import org.semanticweb.kaon2.api.DefaultOntologyResolver;
 import org.semanticweb.kaon2.api.KAON2Exception;
 import org.semanticweb.kaon2.api.KAON2Manager;
@@ -24,8 +25,6 @@ import org.semanticweb.kaon2.api.Ontology;
 import org.semanticweb.kaon2.api.OntologyManager;
 import org.semanticweb.kaon2.api.logic.Rule;
 import org.semanticweb.kaon2.api.reasoner.Reasoner;
-
-import uk.ac.ox.cs.pdq.fol.Dependency;
 
 public class ExecutorOWL {
 
@@ -42,7 +41,7 @@ public class ExecutorOWL {
 		logger.setLevel(level);
 		logger.setUseParentHandlers(false);
 
-		System.out.println("Starting the KAON2-GSat Executor (from OWL)...");
+		System.out.println("Starting the KAON2 Executor (from OWL)...");
 
 		try {
 			Class.forName("uk.ac.ox.cs.pdq.fol.TGD");
@@ -54,16 +53,13 @@ public class ExecutorOWL {
 		try {
 
 			if (args.length > 0)
-				if (args[0].equals("compare"))
-					if (args.length == 3)
-						if (args[2].matches("\\d+"))
-							compare(args[1], Integer.parseInt(args[2]));
+					if (args.length == 2)
+						if (args[1].matches("\\d+"))
+							run(args[0], Integer.parseInt(args[1]));
 						else
 							printHelp("The TIMEOUT must be a positive number!");
 					else
-						printHelp("Wrong number of parameters for compare");
-				else
-					printHelp("Wrong command (i.e. first argument)");
+						printHelp("requires two arguments");
 			else
 				printHelp("No arguments provided");
 
@@ -75,7 +71,7 @@ public class ExecutorOWL {
 
 	}
 
-	private static void compare(String input_file, final int timeout) {
+	private static void run(String input_file, final int timeout) {
 
 		Handler handlerObj = new ConsoleHandler();
 		handlerObj.setLevel(Level.WARNING);
@@ -83,7 +79,6 @@ public class ExecutorOWL {
 		App.logger.setLevel(Level.WARNING);
 		App.logger.setUseParentHandlers(false);
 
-		System.out.println("\n====================");
 		System.out.println("KAON2 Start");
 		final long startTimeKAON2 = System.nanoTime();
 		final Collection<Rule> runKAON2 = new LinkedList<>();
@@ -106,8 +101,12 @@ public class ExecutorOWL {
 			System.out.println("Started..");
 			System.out.println(futureKAON2.get(timeout, TimeUnit.SECONDS));
 			System.out.println("Finished!");
-		} catch (TimeoutException | InterruptedException | ExecutionException e) {
+        } catch (TimeoutException e) {
+            futureKAON2.cancel(true);
+            System.err.println("!!! TIME OUT !!!");
+		} catch (InterruptedException | ExecutionException e) {
 			futureKAON2.cancel(true);
+            System.err.println("!!! ERROR !!!");
 			System.err.println("Terminated!");
 		}
 
@@ -118,56 +117,13 @@ public class ExecutorOWL {
 		System.out.println("KAON2 End; # rules: " + runKAON2.size());
 		System.out.println("KAON2 total time : " + String.format(Locale.UK, "%.0f", totalTimeKAON2 / 1E6) + " ms = "
 				+ String.format(Locale.UK, "%.2f", totalTimeKAON2 / 1E9) + " s");
-		System.out.println("Result: (" + runKAON2.size() + " rules)");
+		System.out.println("Full TGD saturation: (" + runKAON2.size() + " dependecies)");
+
+        System.out.println("\n====================");
 		runKAON2.forEach(rule -> IO.getDatalogRules(rule).forEach(System.out::println));
 		System.out.println("====================");
 
-		System.out.println("\n====================");
-		System.out.println("GSat Start");
-		final long startTimeGSat = System.nanoTime();
-		Collection<GTGD> runGSat = new LinkedList<>();
-		ExecutorService executorGSat = Executors.newSingleThreadExecutor();
-		Future<String> futureGSat = executorGSat.submit(new Callable<String>() {
-			@Override
-			public String call() throws Exception {
-				try {
-					runGSat.addAll(runGSat(input_file));
-				} catch (Exception e) {
-					e.printStackTrace();
-					return "Failed!";
-				}
-				return "Completed!";
-			}
-		});
-
-		try {
-			System.out.println("Started..");
-			System.out.println(futureGSat.get(timeout, TimeUnit.SECONDS));
-			System.out.println("Finished!");
-		} catch (TimeoutException | InterruptedException | ExecutionException e) {
-			futureGSat.cancel(true);
-			System.err.println("Terminated!");
-		}
-
-		executorGSat.shutdownNow();
-
-		final long stopTimeGSat = System.nanoTime();
-		final long totalTimeGSat = stopTimeGSat - startTimeGSat;
-		System.out.println("GSat End; # rules: " + runGSat.size());
-		System.out.println("GSat total time : " + String.format(Locale.UK, "%.0f", totalTimeGSat / 1E6) + " ms = "
-				+ String.format(Locale.UK, "%.2f", totalTimeGSat / 1E9) + " s");
-		System.out.println("Results: (" + runGSat.size() + " rules)");
-		runGSat.forEach(tgd -> IO.getDatalogRules(tgd).forEach(System.out::println));
-		System.out.println("====================");
-
-		System.out.println("\n====================");
-		System.out.println("Summary:" + Paths.get(input_file).getFileName() + ";" + runKAON2.size() + ";"
-				+ String.format(Locale.UK, "%.0f", totalTimeKAON2 / 1E6) + ";" + runGSat.size() + ";"
-				+ String.format(Locale.UK, "%.0f", totalTimeGSat / 1E6));
-		System.out.println("====================");
-
 		System.exit(0); // needed because sometimes the program does not terminate
-
 	}
 
 	private static Collection<Rule> runKAON2(String input_file) throws KAON2Exception, InterruptedException {
@@ -193,13 +149,6 @@ public class ExecutorOWL {
 		return reductionToDLP;
 	}
 
-	private static Collection<GTGD> runGSat(String input_file) throws Exception {
-		OWLIO owlio = new OWLIO(input_file, true);
-		Collection<Dependency> rules = owlio.getRules();
-		Collection<GTGD> runGSat = GSat.getInstance().run(rules);
-		return runGSat;
-	}
-
 	/**
 	 * Prints the help message of the program
 	 * 
@@ -210,10 +159,8 @@ public class ExecutorOWL {
 		System.err.println();
 		System.err.println(message);
 		System.err.println();
-		System.err.println("Note that only these commands are currently supported:");
-		System.err.println("compare \t to compare KAON2 and GSat");
 		System.err.println();
-		System.err.println("if compare is specified the following arguments must be provided, in this strict order:");
+		System.err.println("the following arguments must be provided, in this strict order:");
 		System.err.println("<PATH OF THE OWL FILE> <TIMEOUT (sec)>");
 		System.err.println();
 
