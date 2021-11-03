8bf577e86839429a0cf06e5c7eaaffe13705f484
Maxime Buron <maxime.buron@cs.ox.ac.uk>
2021-11-03 13:55:03 +0000

    install kaon2 script

--- install_kaon2.sh
+++ install_kaon2.sh
@@ -4,10 +4,13 @@ set -e
 
 # go into KAON2 source directory from Boris 
 cd KAON2
-rm -r classes
+rm -fr classes
 mkdir -p classes
 
 javac $(find src -name "*.java") -d classes
 
 cd classes
 jar cf ../kaon2.jar $(find . -name "*.class")
+
+cd ..
+mvn install:install-file -Dfile=kaon2.jar -DgroupId=org.semanticweb.kaon2 -DartifactId=kaon2 -Dversion=2008-06-29 -Dpackaging=jar -DgeneratePom=true
