d19e3523fda4f7f39177d00b1d478096eb7f55e7
Stefano Germano <2128484+stefanogermano@users.noreply.github.com>
2024-09-10 10:38:49 +0200

    Update to JUnit 5

--- gsat-utils/tgd-viz/src/test/java/uk/co/ac/ox/gsat/AppTest.java
+++ gsat-utils/tgd-viz/src/test/java/uk/co/ac/ox/gsat/AppTest.java
@@ -1,8 +1,8 @@
 package uk.co.ac.ox.gsat;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
